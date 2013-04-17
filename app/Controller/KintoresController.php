cfdb7635f3cc0b71f5b88114f269033eb1d1c73e
otukutun <orehaorz@gmail.com>
2013-04-17 10:54:11 +0900

    repair function which upload. added function thumbnail

--- app/Controller/KintoresController.php
+++ app/Controller/KintoresController.php
@@ -56,107 +56,120 @@ class KintoresController extends AppController {
  * @return void
  */
 	public function add() {
-		if ($this->request->is('post')) {
-                $this->Kintore->create();
-                $this->request->data['Kintore']['file'] = $this->Kintore->fileupload($this->request->data['Kintore']['file']);
+            if ($this->request->is('post')) {
+                    $this->Kintore->create();
+                    $this->request->data['Kintore']['file'] = $this->Kintore->fileUpload($this->request->data['Kintore']['file']);
 
-			if ($this->Kintore->save($this->request->data)) {
-				$this->Session->setFlash(
-					__('The %s has been saved', __('kintore')),
-					'alert',
-					array(
-						'plugin' => 'TwitterBootstrap',
-						'class' => 'alert-success'
-					)
-				);
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(
-					__('The %s could not be saved. Please, try again.', __('kintore')),
-					'alert',
-					array(
-						'plugin' => 'TwitterBootstrap',
-						'class' => 'alert-error'
-					)
-				);
-			}
-		}
-		$categories = $this->Kintore->Category->find('list');
-		$this->set(compact('categories'));
-	}
+                    if ($this->request->data['Kintore']['file'] === false) {//ファイルがアップロードされなかった場合の処理
+                            $this->Session->setFlash(
+                                    __('The %s could not be saved. Please, try again.', __('kintore')),
+                                    'alert',
+                                    array(
+                                            'plugin' => 'TwitterBootstrap',
+                                            'class' => 'alert-success'
+                                    )
+                            );
+                            $this->redirect(array('action' => 'index'));
+                    } //endif
+                    $this->Kintore->makeThumbnail($this->request->data['Kintore']['file']);
 
-/**
- * edit method
- *
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		$this->Kintore->id = $id;
-		if (!$this->Kintore->exists()) {
-			throw new NotFoundException(__('Invalid %s', __('kintore')));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Kintore->save($this->request->data)) {
-				$this->Session->setFlash(
-					__('The %s has been saved', __('kintore')),
-					'alert',
-					array(
-						'plugin' => 'TwitterBootstrap',
-						'class' => 'alert-success'
-					)
-				);
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(
-					__('The %s could not be saved. Please, try again.', __('kintore')),
-					'alert',
-					array(
-						'plugin' => 'TwitterBootstrap',
-						'class' => 'alert-error'
-					)
-				);
-			}
-		} else {
-			$this->request->data = $this->Kintore->read(null, $id);
-		}
-		$categories = $this->Kintore->Category->find('list');
-		$this->set(compact('categories'));
-	}
+                    if ($this->Kintore->save($this->request->data)) {
+                            $this->Session->setFlash(
+                                    __('The %s has been saved', __('kintore')),
+                                    'alert',
+                                    array(
+                                            'plugin' => 'TwitterBootstrap',
+                                            'class' => 'alert-success'
+                                    )
+                            );
+                            $this->redirect(array('action' => 'index'));
+                    } else {
+                            $this->Session->setFlash(
+                                    __('The %s could not be saved. Please, try again.', __('kintore')),
+                                    'alert',
+                                    array(
+                                            'plugin' => 'TwitterBootstrap',
+                                            'class' => 'alert-error'
+                                    )
+                            );
+                    }
+            }
+            $categories = $this->Kintore->Category->find('list');
+            $this->set(compact('categories'));
+    }
 
-/**
- * delete method
- *
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Kintore->id = $id;
-		if (!$this->Kintore->exists()) {
-			throw new NotFoundException(__('Invalid %s', __('kintore')));
-		}
-		if ($this->Kintore->delete()) {
-			$this->Session->setFlash(
-				__('The %s deleted', __('kintore')),
-				'alert',
-				array(
-					'plugin' => 'TwitterBootstrap',
-					'class' => 'alert-success'
-				)
-			);
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(
-			__('The %s was not deleted', __('kintore')),
-			'alert',
-			array(
-				'plugin' => 'TwitterBootstrap',
-				'class' => 'alert-error'
-			)
-		);
-		$this->redirect(array('action' => 'index'));
-	}
+    /**
+     * edit method
+     *
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+            $this->Kintore->id = $id;
+            if (!$this->Kintore->exists()) {
+                    throw new NotFoundException(__('Invalid %s', __('kintore')));
+            }
+            if ($this->request->is('post') || $this->request->is('put')) {
+                    if ($this->Kintore->save($this->request->data)) {
+                            $this->Session->setFlash(
+                                    __('The %s has been saved', __('kintore')),
+                                    'alert',
+                                    array(
+                                            'plugin' => 'TwitterBootstrap',
+                                            'class' => 'alert-success'
+                                    )
+                            );
+                            $this->redirect(array('action' => 'index'));
+                    } else {
+                            $this->Session->setFlash(
+                                    __('The %s could not be saved. Please, try again.', __('kintore')),
+                                    'alert',
+                                    array(
+                                            'plugin' => 'TwitterBootstrap',
+                                            'class' => 'alert-error'
+                                    )
+                            );
+                    }
+            } else {
+                    $this->request->data = $this->Kintore->read(null, $id);
+            }
+            $categories = $this->Kintore->Category->find('list');
+            $this->set(compact('categories'));
+    }
+
+    /**
+     * delete method
+     *
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+            if (!$this->request->is('post')) {
+                    throw new MethodNotAllowedException();
+            }
+            $this->Kintore->id = $id;
+            if (!$this->Kintore->exists()) {
+                    throw new NotFoundException(__('Invalid %s', __('kintore')));
+            }
+            if ($this->Kintore->delete()) {
+                    $this->Session->setFlash(
+                            __('The %s deleted', __('kintore')),
+                            'alert',
+                            array(
+                                    'plugin' => 'TwitterBootstrap',
+                                    'class' => 'alert-success'
+                            )
+                    );
+                    $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(
+                    __('The %s was not deleted', __('kintore')),
+                    'alert',
+                    array(
+                            'plugin' => 'TwitterBootstrap',
+                            'class' => 'alert-error'
+                    )
+            );
+            $this->redirect(array('action' => 'index'));
+    }
 }
--- app/Model/Kintore.php
+++ app/Model/Kintore.php
@@ -54,18 +54,37 @@ class Kintore extends AppModel {
                 )
         );
 
-        public function fileupload($file) {//画像アップロード機能
+        public function fileUpload($file) {//画像アップロード機能
                 if (is_uploaded_file($file['tmp_name'])) {//ファイルのアップロード処理
-                        $image = time() . '.' . substr(strrchr($file['name'],'.'),1);
-                        $file_path = 'img' . DS . 'musules' . DS . $image;
-                        $file_return_path = 'musules' . DS . $image;
-                        if (move_uploaded_file($file['tmp_name'],$file_path)) {
-                            return $file_return_path;
+                        $upload_dir = IMAGES_URL . 'musules' . DS;//upload先ディレクトリ
+                        $upload_file = time() . '.' . substr(strrchr($file['name'],'.'),1);//uploadファイル名
+                        if (move_uploaded_file($file['tmp_name'], $upload_dir . $upload_file)) {
+                            //return 'musules' . DS . $upload_file;
+                            return $upload_file;
                         } else {
-                                return 'error';
+                                return false;
                         }
 
                 }
 
-        }
+        }//endfunction
+
+        public function makeThumbnail($file_path) {//サムネイル画像作成機能
+                if (file_exists(IMAGES . 'musules' . DS . $file_path)) {//ファイルの確認
+                        try {
+                                if (!file_exists(IMAGES . 'thumbnails')) {//サムネイル保存ディレクトリの有無の確認
+                                        mkdir(IMAGES . 'thubmnails',0777);
+                                }
+                                $im = new Imagick(IMAGES . 'musules' . DS . $file_path);
+                                $im->thumbnailImage(200,150);
+                                $im->writeImage(IMAGES . 'thumbnails' . DS . $file_path);
+                                $im->destroy();
+                                return true;
+
+                        } catch (Exception $e) {
+                                return false;
+                        }//endtry
+                } //endif
+                return false;
+        }//endfunction
 }
